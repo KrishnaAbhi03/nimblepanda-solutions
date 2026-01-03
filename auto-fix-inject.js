2a05ace703f418239358373101a66fa532a74d68
Abhinav Yadav <developerabhinav29@gmail.com>
2026-01-03 16:36:09 +0530

    Create auto-injection script that loads color-fix on all pages

---
+++ auto-fix-inject.js
@@ -0,0 +1,7 @@
+// Auto-injection script - Place this in existing script tags
+(function() {
+    const script = document.createElement('script');
+    script.src = 'color-fix.js';
+    script.async = false;
+    document.head.insertBefore(script, document.head.firstChild);
+})();
